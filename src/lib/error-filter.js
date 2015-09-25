05b85e642b8e11f46b36a16e7b7b4b165bafa6f7
Soares Chen <soares.chen@gmail.com>
2015-09-25 18:51:28 +0800

    Add error filter component.

---
+++ src/lib/error-filter.js
@@ -0,0 +1,39 @@
+import { assertFunction } from 'quiver-util/assert'
+import { StreamFilter } from 'quiver-component-basic'
+import { componentConstructor } from 'quiver-component-base/util'
+
+const errorToStreamFilter = errorHandler =>
+  (config, handler) =>
+    (args, streamable) =>
+      handler(args, streamable)
+      .catch(errorHandler)
+
+export class ErrorFilter extends StreamFilter {
+  streamFilterFn() {
+    const errorHandler = this.errorHandlerFn()
+    return errorToStreamFilter(errorHandler)
+  }
+
+  errorHandlerFn() {
+    throw new Error('abstract method errorHandlerFn() is not implemented')
+  }
+}
+
+const safeErrorHandlerFn = errorHandler => {
+  assertFunction(errorHandler)
+
+  return async function(err) {
+    const resultStreamable = await errorHandler(err)
+    assertFunction(resultStreamable.toStream,
+      'recovered result must be streamable')
+
+    return resultStreamable
+  }
+}
+
+export const errorFilter = componentConstructor(
+  ErrorFilter, 'errorHandlerFn', safeErrorHandlerFn)
+
+export const filterError = function(errorHandler) {
+  return this.addMiddleware(errorFilter(errorHandler))
+}
